665e71a63f35644e2a1f9a935edf085dc83f10e4
Maria Mezhova <maria.mezhova@yahoo.com>
2023-07-18 14:49:05 +0700

    open appropriate vc on notification tap

--- Struyach Fitness/Other/SceneDelegate.swift
+++ Struyach Fitness/Other/SceneDelegate.swift
@@ -11,6 +11,7 @@ import IQKeyboardManagerSwift
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
+    var originalNavigationStack: [UIViewController] = []
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
@@ -30,8 +31,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             } else {
                 // user is signed in but there is no user data for him or her, need to logout user and present loginVC
                 print ("user is signed in but there is no user data for him or her, need to logout user and present loginVC")
-                AuthManager.shared.signOut { [weak self] success in
-                    guard let self = self else {return}
+                AuthManager.shared.signOut { success in
                     if success {
                         let signInVC = LoginViewController()
                         signInVC.navigationItem.largeTitleDisplayMode = .never
@@ -51,6 +51,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             navVC.navigationBar.prefersLargeTitles = false
             vc = navVC
         }
+        
+        if let navVC = vc as? UINavigationController {
+                   originalNavigationStack = navVC.viewControllers
+               }
+        
         window.rootViewController = vc
         window.makeKeyAndVisible()
         self.window = window
@@ -123,43 +128,59 @@ extension SceneDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
         print ("responding to notification tap")
         let notificationInfo = response.notification.request.content.userInfo
-
-            let navVC = UINavigationController()
-
-            if let pushType = notificationInfo["notificationType"] as? String {
-                print(pushType)
-                switch pushType {
-                case "postComment":
-                    if let postId = notificationInfo["destination"] as? String {
-                        DatabaseManager.shared.fetchBlogPost(postId: postId) { post in
-                            if let post = post {
-                                let commentsVC = CommentsViewController(blogPost: post)
-                                navVC.pushViewController(commentsVC, animated: true)
+        let navVC = UINavigationController()
+        let tabBarController = TabBarController()
+        window?.rootViewController = tabBarController
+        if let pushType = notificationInfo["notificationType"] as? String {
+            print(pushType)
+            switch pushType {
+            case "postComment":
+                if let postId = notificationInfo["destination"] as? String {
+                    DatabaseManager.shared.fetchBlogPost(postId: postId) { post in
+                        if let post = post {
+                            let commentsVC = CommentsViewController(blogPost: post)
+                            commentsVC.title = "Comments".localized()
+                            commentsVC.navigationItem.backButtonTitle = "Back".localized()
+                            tabBarController.selectedIndex = 1
+                            if let blogVC = tabBarController.selectedViewController as? UINavigationController {
+                                blogVC.pushViewController(commentsVC, animated: true)
                             }
                         }
-                    }
-                case "workoutComment":
-                    if let workoutId = notificationInfo["destination"] as? String,
-                       let programId = notificationInfo["collectionId"] as? String {
-                        DatabaseManager.shared.fetchWorkout(programId: programId, workoutId: workoutId) { workout in
-                            if let workout = workout {
-                                let commentsVC = CommentsViewController(workout: workout)
-                                navVC.pushViewController(commentsVC, animated: true)
-                            }
-                        }
-                    }
-                default:
-                    if let programName = notificationInfo["destination"] as? String {
-                        let selectedProgramVC = WorkoutsViewController()
-                        selectedProgramVC.title = programName
-                        navVC.pushViewController(selectedProgramVC, animated: true)
+                        //                            navVC.pushViewController(commentsVC, animated: true)
                     }
                 }
-            } else {
-                print("Error receiving notification type")
+            case "workoutComment":
+                if let workoutId = notificationInfo["destination"] as? String,
+                   let programId = notificationInfo["collectionId"] as? String {
+                    DatabaseManager.shared.fetchWorkout(programId: programId, workoutId: workoutId) { workout in
+                        if let workout = workout {
+                            let commentsVC = CommentsViewController(workout: workout)
+                            commentsVC.title = "Comments".localized()
+                            commentsVC.navigationItem.backButtonTitle = "Back".localized()
+                            tabBarController.selectedIndex = 0
+                            if let programsVC = tabBarController.selectedViewController as? UINavigationController {
+                                let workoutVC = WorkoutsViewController()
+                                workoutVC.title = programId.localized()
+                                programsVC.pushViewController(workoutVC, animated: true)
+                                programsVC.pushViewController(commentsVC, animated: true)
+                            }
+                        }
+                    }
+                }
+            default:
+                if let programName = notificationInfo["destination"] as? String {
+                    let selectedProgramVC = WorkoutsViewController()
+                    selectedProgramVC.title = programName
+                    navVC.pushViewController(selectedProgramVC, animated: true)
+                }
             }
-
-            window?.rootViewController = navVC
+        } else {
+            print("Error receiving notification type")
         }
+        
+//        if AuthManager.shared.isSignedIn {
+//            window?.rootViewController = navVC
+//        }
+    }
 }
 
